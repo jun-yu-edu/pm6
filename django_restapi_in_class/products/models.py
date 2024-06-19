5189e93f9053a4c3333d4e4ce79e74fd0520afe9
jun <junyu.edu@gmail.com>
2024-06-19 13:15:26 +0900

    a

--- django_restapi_in_class/products/models.py
+++ django_restapi_in_class/products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    stock = models.IntegerField()
+    register_date = models.DateField()
--- django_restapi_in_class/products/views.py
+++ django_restapi_in_class/products/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from articles.models import Article
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from articles.serializers import ArticleSerializer
+
+@api_view(['GET', 'POST'])
+def article_list(request):
+
+    if request.method == 'GET':
+    
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        
+        return Response(serializer.data)
+    
+    
+    elif request.method == 'POST':
+    
+        data = request.data
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            
+            return Response(serializer.data)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, id):
+
+    if request.method == 'GET':
+    
+        article = Article.objects.get(id=id)
+        serialzer = ArticleSerializer(article)
+
+        return Response(serialzer.data)
+    
+    
+    elif request.method == 'PUT':
+        data = request.data
+        article = Article.objects.get(id=id)
+        serialzer = ArticleSerializer(article, data=data, partial=True)
+
+        if serialzer.is_valid(raise_exception=True):
+            serialzer.save()
+            
+            return Response(serialzer.data)
+            
+            
+    elif request.method == 'DELETE':
+    
+        article = Article.objects.get(id=id)
+        article.delete()
+        
+        return Response(status=204)
